3dc7edf6f8fa4332b0b666a470d2d32bbb2e5626
Matjaž Drolc <mdrolc@gmail.com>
2019-11-25 21:05:43 +0100

    feat: Put together test for parsing data into a TimestampedWeatherMap data structure

--- src/meteo_data/radar.rs
+++ src/meteo_data/radar.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 extern crate reqwest;
+use regex::Regex;
 
 #[derive(Serialize, Deserialize, std::fmt::Debug, std::cmp::PartialEq)]
 struct RadarPrecipitationResponseItem {
@@ -27,6 +28,61 @@ fn parse_json(
     Ok(serde_json::from_str(r.as_str())?)
 }
 
+fn parse_datetime(
+    dt: &str,
+) -> std::result::Result<chrono::DateTime<chrono::FixedOffset>, chrono::ParseError> {
+    chrono::DateTime::parse_from_rfc3339(dt)
+}
+
+fn parse_bbox(s: &str) -> std::result::Result<Bbox, std::num::ParseFloatError> {
+    let re = Regex::new(
+        r"^(?P<lat1>\d+\.\d+),(?P<lon1>\d+\.\d+),(?P<lat2>\d+\.\d+),(?P<lon2>\d+\.\d+)$",
+    )
+    .unwrap();
+    let caps = re.captures(s).unwrap();
+    Ok(Bbox {
+        lat1: caps.name("lat1").unwrap().as_str().parse()?,
+        lon1: caps.name("lon1").unwrap().as_str().parse()?,
+        lat2: caps.name("lat2").unwrap().as_str().parse()?,
+        lon2: caps.name("lon2").unwrap().as_str().parse()?,
+    })
+}
+
+fn load_radar(
+    site: Box<dyn super::site::SiteDownloader>,
+) -> std::result::Result<Vec<TimestampedWeatherMap>, Box<dyn std::error::Error>> {
+    let json = site.get_radar_meta()?;
+    let response_items = parse_json(json)?;
+    Ok(response_items
+        .iter()
+        .map(
+            |item| -> std::result::Result<TimestampedWeatherMap, Box<dyn std::error::Error>> {
+                Ok(TimestampedWeatherMap {
+                    datetime: parse_datetime(item.valid.as_str())?,
+                    weather_map: WeatherMap {
+                        bbox: parse_bbox(item.bbox.as_str())?,
+                        image: image::io::Reader::new(std::io::Cursor::new(
+                            site.get_radar_image(item.path.as_str())?,
+                        ))
+                        .with_guessed_format()?
+                        .decode()?
+                        .to_rgba(),
+                    },
+                })
+            },
+        )
+        .filter(|item| match item {
+            Ok(v) => true,
+            _ => false,
+        })
+        .map(|item| item.unwrap())
+        .collect())
+}
+
+struct TimestampedWeatherMap {
+    datetime: chrono::DateTime<chrono::FixedOffset>,
+    weather_map: WeatherMap,
+}
 
 struct WeatherMap {
     //  datetime: chrono::DateTime<chrono::FixedOffset>,
@@ -39,6 +95,7 @@ struct WeatherMap {
 mod tests {
     use super::parse_json;
     use super::RadarPrecipitationResponseItem;
+    use chrono::offset::TimeZone;
 
     #[test]
     fn test_dummy() {
@@ -61,5 +118,19 @@ mod tests {
                 valid: "2019-11-21T02:45:00Z".to_string()
             }]
         );
+    }
+
+    #[test]
+    fn test_load_radar() {
+        let site = super::super::site::DummySiteDownloader::new();
+        let mut result = super::load_radar(Box::new(site)).unwrap();
+        assert_eq!(result.len(), 1);
+        let first_timestamped_weather_map = result.pop().unwrap();
+        assert_eq!(
+            first_timestamped_weather_map.datetime,
+            chrono::FixedOffset::east(0)
+                .ymd(2019, 11, 21)
+                .and_hms(02, 45, 00)
+        )
     }
 }
--- src/meteo_data/site.rs
+++ src/meteo_data/site.rs
@@ -5,35 +5,35 @@ use std::io::BufReader;
 use std::io::Read;
 use std::result::Result;
 
-trait SiteDownloader {
-    fn get_radar_meta() -> Result<String, Box<dyn Error>>;
-    fn get_radar_image(rel_path: &str) -> Result<Vec<u8>, Box<dyn Error>>;
+pub trait SiteDownloader {
+    fn get_radar_meta(&self) -> Result<String, Box<dyn Error>>;
+    fn get_radar_image(&self, rel_path: &str) -> Result<Vec<u8>, Box<dyn Error>>;
 }
 
-struct MeteoArsoGovSiDownloader;
-struct DummySiteDownloader;
+pub struct MeteoArsoGovSiDownloader;
+pub struct DummySiteDownloader;
 
 impl MeteoArsoGovSiDownloader {
-    fn new() -> MeteoArsoGovSiDownloader {
+    pub fn new() -> MeteoArsoGovSiDownloader {
         MeteoArsoGovSiDownloader {}
     }
 }
 
 impl DummySiteDownloader {
-    fn new() -> DummySiteDownloader {
+    pub fn new() -> DummySiteDownloader {
         DummySiteDownloader {}
     }
 }
 
 impl SiteDownloader for MeteoArsoGovSiDownloader {
-    fn get_radar_meta() -> Result<String, Box<dyn Error>> {
+    fn get_radar_meta(&self) -> Result<String, Box<dyn Error>> {
         Ok(reqwest::get(
             "http://www.meteo.si/uploads/probase/www/nowcast/inca/inca_si0zm_data.json?prod=si0zm",
         )?
         .text()?)
     }
 
-    fn get_radar_image(rel_path: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+    fn get_radar_image(&self, rel_path: &str) -> Result<Vec<u8>, Box<dyn Error>> {
         let mut resp = reqwest::get(format!("{}{}", "http://www.meteo.si", rel_path).as_str())?;
         let mut buf: Vec<u8> = vec![];
         resp.copy_to(&mut buf)?;
@@ -42,11 +42,11 @@ impl SiteDownloader for MeteoArsoGovSiDownloader {
 }
 
 impl SiteDownloader for DummySiteDownloader {
-    fn get_radar_meta() -> Result<String, Box<dyn Error>> {
+    fn get_radar_meta(&self) -> Result<String, Box<dyn Error>> {
         Ok(String::from("[{\"mode\":\"ANL\",\"path\":\"0.png\",\"date\":\"201911210245\",\"hhmm\":\"0245\",\"bbox\":\"44.67,12.1,47.42,17.44\",\"width\":\"800\",\"height\":\"600\",\"valid\":\"2019-11-21T02:45:00Z\"}]"))
     }
 
-    fn get_radar_image(rel_path: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+    fn get_radar_image(&self, rel_path: &str) -> Result<Vec<u8>, Box<dyn Error>> {
         let file = File::open("test_fixtures/inca_si0zm_20191115-1830+0000.png")?;
         let mut buf_reader = BufReader::new(file);
         let mut contents = Vec::new();
